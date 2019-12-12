05861c6d0885f9a637cb1a0eb5deae3d084b680f
WhiskyCat <c_koenig15@cs.uni-kl.de>
2019-12-12 14:12:01 +0100

    hotfix dataload also fixes some issues with eventlistener and references

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,8 @@ document.addEventListener('deviceready', function(){
     let asignatura_name = null;
     let asignatura = null;
     let text = null;
+    let chosen_asignatura = null;
+    let class_objects = [];
 
     getRedirectResult();
     document.querySelector('#btn_google_login').addEventListener('click', function(){
@@ -91,7 +93,9 @@ document.addEventListener('deviceready', function(){
                     tr.appendChild(dl);
 
                     td.addEventListener('click',()=> {
-                        toClassInterface(td.innerText, child_snapshot);
+                        console.log("Key tbl: " + td.id.split("_")[1]);
+                        console.log("Key snapshot: " + child_snapshot.key);
+                        toClassInterface(td.innerText, td.id.split("_")[1]);
                     });
 
                     btn_del.addEventListener('click', function(event){
@@ -118,11 +122,13 @@ document.addEventListener('deviceready', function(){
      * and showing the class elements
      *
      * @param classname
-     * @param snapshot
+     * @param key
      */
-    function toClassInterface(classname, snapshot){
-        load_student_data(snapshot.key);
-        load_horario_data(snapshot.key);
+    function toClassInterface(classname, key){
+        chosen_asignatura = key;
+        load_student_data(key);
+        load_horario_data(key);
+
         document.getElementById("title").textContent = "Asignatura";
         document.getElementById("btn_nueva_asig").style.display ="none";
         document.getElementById("header_table").style.display ="none";
@@ -140,35 +146,7 @@ document.addEventListener('deviceready', function(){
 
         //Sets implementation for the add class popUp
         let submit_class = document.getElementById("form2submit");
-        submit_class.addEventListener('click', () => {
-            ref = db.ref("users/" + user.uid + "/asignaturas/"+ asignatura + "/lista horarios/");
-            let date = document.getElementById("form2input1").value;
-            let table = document.getElementById("table_classes_participants");
-            let students = [];
-            for(let i = 1; i < table.rows.length; i++){
-                students.push({
-                    Alumno: table.rows[i].cells[1].innerHTML,
-                    Presentado: "NO"
-                });
-            }
-            let clase = {
-            Mes:  date.split("/")[0],
-            Dia: date.split("/")[1],
-            Hora: date.split("/")[2],
-            Aula: document.getElementById("form2input2").value,
-            QR: create_qr(),
-            Presencia: students
-        };
-           ref.push(clase);
-
-           //Updates the table by retrieving the entries, including the new one from the server
-            // To prevent duplications the table will be emptied first
-           let tempEmptyTable = doc.getElementById("table_classes");
-           for(let i = 1; i < tempEmptyTable.rows.length; i++){
-               tempEmptyTable.deleteRow(1);
-           }
-           load_horario_data(asignatura);
-        });
+        submit_class.addEventListener('click', add_class);
 
             document.querySelector('#page_main').style.display = 'block';
             document.querySelector('#page_login').style.display = 'none';
@@ -179,6 +157,39 @@ document.addEventListener('deviceready', function(){
         back_to_aula.addEventListener('click', () => {
             toAulaGlobalInterface();
         });
+    }
+
+    function add_class() {
+        ref = db.ref("users/" + user.uid + "/asignaturas/"+ chosen_asignatura + "/lista horarios/");
+        console.log("Key add_class: " + chosen_asignatura);
+        let date = document.getElementById("form2input1").value;
+        let table = document.getElementById("table_classes_participants");
+        let students = [];
+        for(let i = 1; i < table.rows.length; i++){
+            students.push({
+                Alumno: table.rows[i].cells[1].innerHTML,
+                Presentado: "NO"
+            });
+        }
+        let clase = {
+            Mes:  date.split("/")[0],
+            Dia: date.split("/")[1],
+            Hora: date.split("/")[2],
+            Aula: document.getElementById("form2input2").value,
+            QR: create_qr(),
+            Presencia: students
+        };
+        console.log("Before push");
+        ref.push(clase);
+
+        //Updates the table by retrieving the entries, including the new one from the server
+        //To prevent duplications the table will be emptied first
+        let tempEmptyTable = document.getElementById("table_classes");
+        let border = tempEmptyTable.rows.length;
+        for(let i = 1; i < border; i++){
+            tempEmptyTable.deleteRow(1);
+        }
+        load_horario_data(chosen_asignatura);
     }
 
     function create_qr() {
@@ -200,7 +211,6 @@ document.addEventListener('deviceready', function(){
             snapshot.forEach(function () {
                 entries.push(snapshot.val());
             });
-            let other = [];
 
             let table = document.getElementById("table_classes");
 
@@ -256,6 +266,7 @@ document.addEventListener('deviceready', function(){
                 popUpContainer.appendChild(divPopup);
 
                 document.getElementById("class_page").appendChild(popUpContainer);
+                class_objects.push(popUpContainer);
                 let open_alumnos = document.createElement("a");
                 open_alumnos.href = "#alumnos_nr" + i;
                 open_alumnos.text = "Mostrar";
@@ -313,6 +324,9 @@ document.addEventListener('deviceready', function(){
         document.getElementById("add_class").style.display ="none";
         document.getElementById("participants").style.display ="none";
         document.getElementById("to_aula").style.display ="none";
+        let submit_class = document.getElementById("form2submit");
+        submit_class.removeEventListener('click', add_class);
+
 
         let table = document.getElementById("table_classes_participants");
         let border = table.rows.length;
@@ -323,8 +337,10 @@ document.addEventListener('deviceready', function(){
         let table_part = document.getElementById("table_classes");
         border = table_part.rows.length;
         for(let i = 1; i < border; i++){
+            document.getElementById("class_page").removeChild(class_objects[i-1]);
             table_part.deleteRow(1);
         }
+        class_objects = [];
 
         document.getElementById("title").textContent = "Aula Global";
         document.getElementById("btn_nueva_asig").style.display ="block";
